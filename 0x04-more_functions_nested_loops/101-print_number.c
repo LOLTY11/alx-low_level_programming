035ea031759a400f8f4a2421f10c29a8cd54ec56
LawrenceManganyi <manganyiln@gmail.com>
2023-02-24 16:33:40 +0200

    101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,13 +8,13 @@
 
 int main(void)
 {
-	print_number(98);
+	print_numbers(98);
 	_putchar('\n');
 	print_number(402);
 	_putchar('\n');
 	print_number(1024);
 	_putchar('\n');
-	print_number(-98));
+	print_number(-98);
 	_putchar('\n');
 	return (0);
 }
