f721b1c16eae58ebaafc323bae1f29a3edc74e9e
LawrenceManganyi <manganyiln@gmail.com>
2023-03-06 15:36:15 +0200

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -2,8 +2,9 @@
 #include <stdio.h>
 
 /**
- * main - print the numbers from 1 to 100, followed by a new line
- * but lfr multiples of three prints Fizz instead of the number 
+ * main - print the numbers from 1 to 100
+ * followed by a new line
+ * but lfr multiples of three prints Fizz instead of the number
  * and for the multiples of five prints Buzz
  * Return: Always 0 (Success)
  */
