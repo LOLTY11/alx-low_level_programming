845c7aec4c815d5fd8b6c8a9646c2fb24e7cf410
LawrenceManganyi <manganyiln@gmail.com>
2023-03-06 12:11:29 +0200

    104-print_buffer.c

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -3,8 +3,8 @@
 
 /**
  * print_buffer - Prints a buffer 10 bytes at a time, starting with
- * 	the byte position, then showing the hex content,
- * 	then displying printable characters
+ *	the byte position, then showing the hex content,
+ *	then displying printable characters
  * @b: The buffer to be printed.
  * @size: The number of bytes to be printed from the buffer.
  */
