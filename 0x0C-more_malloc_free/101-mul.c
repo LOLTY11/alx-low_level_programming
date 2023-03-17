f426566447b136caaaf727f3a088e9c50007f014
LawrenceManganyi <manganyiln@gmail.com>
2023-03-17 02:55:13 +0200

    101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -13,6 +13,7 @@ int main(int argc, char *argv[])
 {
 	unsigned long mul;
 	int i, j;
+
 	if (argc != 3)
 	{ printf("Error\n");
 	exit(98); }
@@ -25,7 +26,7 @@ int main(int argc, char *argv[])
 			exit (98); }
 		}
 	}
-	mul = atol(argv[1]) *atol(argv[2]);
+	mul = atol(argv[1]) * atol(argv[2]);
 	printf("%lu\n", mul);
 	return (0);
 }
