79b5c2916a15db2160b53e1926e64ca1ec875932
LOLTY11 <manganyiln@gmail.com>
2023-05-09 03:05:31 +0200

    main.h

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -7,18 +7,18 @@
  * Return: w - numbers of bytes printed and read
  * 	   0 - function fail or NULL
  */
-ssize_t read_textfile(const char *filename, size_t letterw){
+ssize_t read_textfile(const char *filename, size_t letters){
 char *buf;
 ssize_t fd;
 ssize_t w;
 ssize_t t;
 
-fd = open(filename, O_RONLY);
+fd = open(filename, O_RDONLY);
 if (fd == -1)
 	return (0);
-buf = malloc(sizeof(char) *letters);
+buf = malloc(sizeof(char) *letter);
 t = read(fd, buf, letters);
-w = writes(STDOUT_FILENO, buf, t);
+w = write(STDOUT_FILENO, buf, t);
 
 free(buf);
 close(fd);
