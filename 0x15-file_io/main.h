98c3f7e0fc67747299321ba5440876bbef813efc
LOLTY11 <manganyiln@gmail.com>
2023-05-09 02:53:11 +0200

    main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -2,7 +2,7 @@
 #define MAIN_H
 #include <sys/types.h>
 #include <sys/stat.h>
-#include <fntl.h>
+#include <fcntl.h>
 #include <unistd.h>
 
 ssize_t read_testfile(cost char *filename, size_t letters);int create_file(const char *filename, char *text_content);
