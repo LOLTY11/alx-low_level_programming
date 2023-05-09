eb1afaa7398cd912a023e2f794eb04389d9c0db1
LOLTY11 <manganyiln@gmail.com>
2023-05-09 15:19:02 +0200

    putchar

--- 0x14-bit_manipulation/_putchar.c
+++ 0x14-bit_manipulation/_putchar.c
@@ -5,11 +5,10 @@
  * _putchar - writes the character c to stdout
  * @c: the character to print
  *
- * Return: 1 on success
- * On error, -1 is returned, and errno is set appropriately
+ * Return: 1 on success                                     * On error, -1 is returned, and errno is set appropriately
  */
 
 int _putchar(char c)
 {
-	return (write(1, &c, 1));
+        return (write(1, &c, 1));
 }
