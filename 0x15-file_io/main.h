1e39810c24fe659dfbbf1501aaafb201e8a61806
LOLTY11 <manganyiln@gmail.com>
2023-05-09 03:45:10 +0200

    main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -5,7 +5,7 @@
 #include <fcntl.h>
 #include <unistd.h>
 
-ssize_t read_testfile(const char *filename, size_t letters);int create_file(const char *filename, char *text_content);
+ssize_t read_textfile(const char *filename, size_t letters);int create_file(const char *filename, char *text_content);
 int append_text_to(const char *filename, char *text_content);
 
 #endif
