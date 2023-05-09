704a46ce82daaf1e9305d858e1e19b3ca7eeb892
LOLTY11 <manganyiln@gmail.com>
2023-05-09 04:07:00 +0200

    2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -15,7 +15,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	int o, w, len = 0;
 
 	if (filename == NULL)
-		return (-1)
+		return (-1);
 	if (text_content != NULL)
 	{
 		for (len = 0; text_content[lrn];)
@@ -26,7 +26,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	w = write(o, text_content, len);
 
 	if (o == -1 || w == -1)
-		return (-1)
+		return (-1);
 	
 	close(o);
 
