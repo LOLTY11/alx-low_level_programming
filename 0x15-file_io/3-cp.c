f26dc342f5df05d13546f415a39f55e20739e385
LOLTY11 <manganyiln@gmail.com>
2023-05-16 01:00:20 +0200

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -68,24 +68,24 @@ int main(int argc, char *argv[])
 	}
 
 	buffer = create_buffer(argv[2]);
-	from = open(argv[1], O_RONLY);
+	from = open(argv[1], O_RDONLY);
 	r = read(from, buffer, 1024);
-	to = open(argv[2], O_CREAT | O_WRONGLY | O_TRUNC, 0664);
+	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	do
 		if (from == -1 || r == -1)
 		{
-			dprint(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1];
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		free(buffer);
 		exit (98);
 	}	
-
+}
 	w = write(to, buffer, r);
 	if (to == -1 || w == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2];
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		free(buffer);
 		exit (99);
-
+	}
 	}
 
 	r = read(from, buffer, 1024);
