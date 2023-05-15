6d5f6c5658c87ec75f4769c7b65b42a0fed467c7
LOLTY11 <manganyiln@gmail.com>
2023-05-16 00:55:56 +0200

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -42,6 +42,7 @@ void close_file(int fd)
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n",fd);
                 exit(100);
 	}
+}
 
 /**
  * main - copies the contents of a file to another file.
@@ -81,9 +82,9 @@ int main(int argc, char *argv[])
 	w = write(to, buffer, r);
 	if (to == -1 || w == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: can't write to %s\n", argv[2];
-	free(buffer);
-	exit (99);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2];
+		free(buffer);
+		exit (99);
 
 	}
 
